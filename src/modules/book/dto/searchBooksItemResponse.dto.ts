0ea5e91a89624c3830ec5f640ddc9430bc7b1fbf
mgyo-qbg <mk.kim@qbgroup.co.kr>
2024-10-29 15:17:33 +0900

    SearchBooksItemResponseDto 에다가 @IsString() 검증 추가

--- src/modules/book/dto/searchBooksItemResponse.dto.ts
+++ src/modules/book/dto/searchBooksItemResponse.dto.ts
@@ -1,8 +1,21 @@
+import { IsString } from 'class-validator';
+
 export class SearchBooksItemResponseDto {
+  @IsString()
   isbn: string;
+
+  @IsString()
   title: string;
+
+  @IsString()
   author: string;
+
+  @IsString()
   publisher: string;
+
+  @IsString()
   image: string;
+
+  @IsString()
   description: string;
 }
