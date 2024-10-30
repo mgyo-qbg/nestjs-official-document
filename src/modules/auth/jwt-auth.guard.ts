6be187e057a95c58accee6fd21424fdfff00d9ab
mgyo-qbg <mk.kim@qbgroup.co.kr>
2024-10-30 09:47:49 +0900

    @public 데코레이터 생성 및 회원가입에 적용 (jwt-auth.guard 인증 비활성화 용도)

--- src/modules/auth/jwt-auth.guard.ts
+++ src/modules/auth/jwt-auth.guard.ts
@@ -1,6 +1,28 @@
-// jwt-auth.guard.ts
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './public.decorator';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context) as
+      | boolean
+      | Promise<boolean>
+      | Observable<boolean>;
+  }
+}
