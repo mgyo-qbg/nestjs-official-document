2f70ba50e301cddde8515dbe64888b2372b2f698
mgyo-qbg <mk.kim@qbgroup.co.kr>
2024-10-30 09:28:35 +0900

    SIGN_IN(로그인) 에 대한 User_Log 추가

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,17 +3,22 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
 import { JwtPayload } from './jwt-payload.interface';
+import { UserWorkName } from '@prisma/client';
+import { UserLogService } from '../user-log/user-log.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly userLogService: UserLogService,
   ) {}
 
   async signIn(email: string, password: string) {
     const user = await this.userService.validateUser(email, password); // 사용자 검증
     const payload: JwtPayload = { id: user.id, email: user.email }; // JWT 페이로드 생성
+    // SIGN_IN 에 대한 UserLog 로그 추가
+    await this.userLogService.InsertUserLog(user.id, UserWorkName.SIGN_IN);
     return {
       accessToken: this.jwtService.sign(payload), // JWT 발급
     };
